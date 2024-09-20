4ac3361dc7a59e1896e583cc1ae82cbfa23827b3
Nestani Datunashvili <nestani.datunashvili@gmail.com>
2024-09-20 19:35:50 +0400

    new

--- index.js
+++ index.js
@@ -1,83 +1,34 @@
-//  1 task-----------------------
+// // task 1   -----------------------------------
 
-// let str = "level";
+// let alfabeticalStr = (str) => str.split("").sort().join("");
 
-// let palindrome = (str) => {
-//   let lowerStr = str.toLowerCase();
-//   for (let i = 0; i < lowerStr.length / 2; i++) {
-//     if (lowerStr[i] !== lowerStr[lowerStr.length - 1 - i]) {
-//       return `${str} is not a palindrome`;
-//     } else {
-//       return `${str} is a palindrome`;
-//     }
-//   }
-// };
-
-// let result = palindrome(str);
+// let result = alfabeticalStr("level");
 
 // console.log(result);
 
-// -----------------------------
+// // task 2   -----------------------------------
 
-// let str = "level";
+// let generateID = (length) => {
+//   const characters =
+//     "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+//   let generatedStr = "";
 
-// let isPlindrome = (str) => {
-//   let lowerStr = str.toLowerCase();
-//   let reverse = str.split("").reverse().join("").toLowerCase();
-
-//   if (lowerStr !== reverse) {
-//     return `${str} is not a palindrome`;
-//   } else {
-//     return `${str} is a palindrome`;
+//   for (let i = 0; i < length; i++) {
+//     let index = Math.floor(Math.random() * characters.length);
+//     generatedStr += characters[index];
 //   }
+
+//   return generatedStr;
 // };
 
-// let result = isPlindrome(str);
+// let result = generateID(5);
 
 // console.log(result);
 
-//  2 task-----------------------
+// // task 3    -----------------------------------
 
-// const fruits = ["Banana", "Orange", "Apple", "Kiwi"];
+// let perfectNum = (num) => {
+//   let isPerfect = false;
 
-// let newFruit = fruits.splice(1, 2);
-// console.log(newFruit);
-
-//  3 task-----------------------
-
-// const fruits = ["Banana", "Orange", "Apple", "Kiwi"];
-
-// let sort = (arr) => {
-//   let alphabeticalArr = arr.sort();
-//   return alphabeticalArr;
+//   if(num)
 // };
-
-// let result = sort(fruits);
-
-// console.log(result);
-
-// 4 task-----------------------
-// let dolphinsScored = [85, 54, 41];
-// let koalasScored = [23, 34, 27];
-
-// let calcAvrg = (score1, score2, score3) => (score1 + score2 + score3) / 3;
-
-// let scoreDolphins = calcAvrg(
-//   dolphinsScored[0],
-//   dolphinsScored[1],
-//   dolphinsScored[2]
-// );
-
-// let scoreKoalas = calcAvrg(koalasScored[0], koalasScored[1], koalasScored[2]);
-
-// let checkWinner = (avgDolphins, avgKoalas) => {
-//   if (avgDolphins >= avgKoalas * 2) {
-//     return `Delfins win ${avgDolphins} vs. ${avgKoalas}),`;
-//   } else if (avgKoalas >= avgDolphins * 2) {
-//     return `Koalas win ${avgKoalas} vs. ${avgDolphins}),`;
-//   } else {
-//     return `  No team wins...`;
-//   }
-// };
-
-// console.log(checkWinner(scoreDolphins, scoreKoalas));
